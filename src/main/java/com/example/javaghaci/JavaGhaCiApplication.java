aa6c31427735220e202309561eecd418e7a969ee
Joakim L. Engeset <joakim.engeset@gmail.com>
2022-08-02 10:29:40 +0200

    Add unused var to see if any checks trigger

--- src/main/java/com/example/javaghaci/JavaGhaCiApplication.java
+++ src/main/java/com/example/javaghaci/JavaGhaCiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaGhaCiApplication {
 
 	public static void main(String[] args) {
+    final String s = "";
 		SpringApplication.run(JavaGhaCiApplication.class, args);
 	}
 
